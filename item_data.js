cb2e47c36067f333851092f51ffa1f939a8bb5d2
Brian Britt <54298777+bmbritt@users.noreply.github.com>
2020-10-14 22:37:51 -0400

    Add files via upload

---
+++ item_data.js
@@ -0,0 +1,8 @@
+// This file will host the master data set of all items to be sold on the website. Functions will receive the data here and filter is as necesary 
+// when determining which products will be displayed.
+
+const products = [{
+    id: 1,
+    name: "Rustic farmhouse table (5 seats)",
+    price: 299.99
+}]
